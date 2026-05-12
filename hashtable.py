1562a3c3a634a0d2b8d162c2a8e4b33b28ab8dd2
JS Ng <ngjunsiang@gmail.com>
2026-05-12 06:51:21 +0000

    hashtablelinearprobing implementation

--- hashtable.py
+++ hashtable.py
@@ -82,7 +82,6 @@ class HashTableLinearProbing(HashTable):
 
     def __init__(self, size: int):
         super().__init__(size)
-        # Add your code here
 
     def __repr__(self) -> str:
         return f"HashTableLinearProbing(size={self.size})"
@@ -92,22 +91,106 @@ class HashTableLinearProbing(HashTable):
 
         If the key already exists in the hash table, the existing value
         is overwritten.
+
+        Note: key and value are stored together as a (key, value) tuple
         """
-        raise NotImplementedError
+        start = _hash_key(key)
+        i = start
+        # Probe until the starting point is reached again (with wraparound)
+        while i + 1 != start:
+            slot = self._data[i]
+            # If slot is unoccupied, store (key, value) pair
+            if slot is None:
+                self._data[i] = (key, value)
+                return
+            stored_key, stored_value = slot  # slot is a (key, value) tuple
+            # If matching key found, overwrite value
+            if key == stored_key:
+                self._data[i] = (key, value)
+                return
+            # Matching key not found; rehash key
+            i = (i + 1) % self.size
+        # If loop exits, no suitable slot found to store key-value pair;
+        # Maybe hash table is full
+        raise KeyError("Hash table is full")
 
     def getitem(self, key: str) -> dict:
         """Retrieves the value associated with key, and returns it.
 
         If the key does not exist, a KeyError is raised.
         """
-        raise NotImplementedError
+        start = _hash_key(key)
+        i = start
+        # Probe until the starting point is reached again (with wraparound)
+        while i + 1 != start:
+            slot = self._data[i]
+            # If slot is unoccupied, key is not found
+            if slot is None:
+                raise KeyError(f"'{key}' not found")
+            stored_key, stored_value = slot  # slot is a (key, value) tuple
+            # If matching key found, return value
+            if key == stored_key:
+                return stored_value
+            # Matching key not found; rehash key
+            i = (i + 1) % self.size
+        # If loop exits, key not found
+        raise KeyError(f"'{key}' not found")
 
     def delitem(self, key: str) -> None:
         """Deletes the key and its associated value from the hash table.
 
         If the key does not exist, a KeyError is raised.
         """
-        raise NotImplementedError
+        start = _hash_key(key)
+        i = start
+        # Probe until the starting point is reached again (with wraparound)
+        while i + 1 != start:
+            slot = self._data[i]
+            # If slot is unoccupied, key is not found
+            if slot is None:
+                raise KeyError(f"'{key}' not found")
+            stored_key, stored_value = slot  # slot is a (key, value) tuple
+            # If matching key found, clear key-value
+            if key == stored_key:
+                self._data[i] = None
+                # This will break subsequent probing; have to check
+                # subsequent key-value pairs
+                self._reinsert_probed_kvpairs(start=i)
+                return
+            # Matching key not found; rehash key
+            i = (i + 1) % self.size
+        # If loop exits, key not found
+        raise KeyError(f"'{key}' not found")
+
+    def _reinsert_probed_kvpairs(self, start: int) -> None:
+        """Method invoked to reinsert probed key-value pairs
+
+        Rationale:
+        When a key-value pair is removed (and replaced with None),
+        key-value pairs that relied on linear probing to be reached are
+        no longer reachable.
+
+        Solution:
+        1. Inspect each key-value pair beginning from start
+        2. Hash the key. If the key hashes to its current location, it
+           does not rely on linear probing [can stop here]
+        3. If the key does not hash to its current location, it relies
+           on linear probing and needs to be re-inserted. [continue checking]
+        4. If an empty slot is found, stop.
+        """
+        i = start
+        while i + 1 != start:
+            slot = self._data[i]
+            if slot is None:  # (4)
+                return
+            key, value = slot  # (1)
+            if _hash_key(key) == i:  # (2)
+                return
+            # (3) Reinsert key-value pair after removal
+            self._data[i] = None
+            self.setitem(key, value)
+            # Continue checking
+            i = (i + 1) % self.size
 
 
 class HashTableSeparateChaining(HashTable):
